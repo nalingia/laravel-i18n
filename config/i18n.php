41f3edabe4e94a77f735cd83ec54b06f0d9783fe
Gianmarco Nalin <gianma.nalin89@gmail.com>
2019-04-16 18:04:27 +0200

    ADD: fields comments.

--- config/i18n.php
+++ config/i18n.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+  /**
+   * The column name identifying catalogue item's locale.
+   */
   'locale_identifier' => 'lang',
+
+  /**
+   * Morph name.
+   */
   'catalogable_morph_name' => 'catalogable',
 ];
